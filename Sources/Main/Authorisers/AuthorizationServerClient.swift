05010bb45cbf4a72b3ef7f10c90b28eb17f4eff1
Rosendahl Sami <sami.rosendahl@reaktor.com>
2024-08-07 15:12:25 +0300

    Revert "[workaround] Remove hard coded openid scope from authorization request;"
    Fixed in Finnish EUDIWalletDemo backend, reference ILO-2988.
    
    This reverts commit c4c40d69976fc51b32577f910454580acca36683.

--- Sources/Main/Authorisers/AuthorizationServerClient.swift
+++ Sources/Main/Authorisers/AuthorizationServerClient.swift
@@ -194,7 +194,7 @@ public actor AuthorizationServerClient: AuthorizationServerClientType {
       responseType: Self.responseType,
       clientId: config.clientId,
       redirectUri: config.authFlowRedirectionURI.absoluteString,
-      scope: scopes.map { $0.value }.joined(separator: " "),
+      scope: scopes.map { $0.value }.joined(separator: " ").appending(" ").appending(Constants.OPENID_SCOPE),
       credentialConfigurationIds: toAuthorizationDetail(credentialConfigurationIds: credentialConfigurationIdentifiers),
       state: state,
       codeChallenge: PKCEGenerator.generateCodeChallenge(codeVerifier: codeVerifier),
